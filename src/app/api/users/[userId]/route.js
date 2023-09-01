8a4e10b3da3860e38c4b849ccecbc5b3142f671d
vancillo <yaredbane@gmail.com>
2023-09-01 00:28:54 -0600

    route handlers params

---
+++ src/app/api/users/[userId]/route.js
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request, {params}) {
+    console.log(request)
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
+    const data = await res.json()
+    return NextResponse.json(data)
+}
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server"
 
-export function GET() {
-  return NextResponse.json({
-    message: "obteniendo datos!"
-  })
+export async function GET() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return NextResponse.json(data)
 }
 
 export function POST() {
