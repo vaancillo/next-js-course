1015225a3abcf33cedd4c893a36b21e0b2ac657f
vancillo <yaredbane@gmail.com>
2023-09-01 21:05:25 -0600

    request body

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -2,10 +2,13 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, {params}) {
     console.log(request)
+
     const {searchParams} = new URL(request.url)
+
     console.log(searchParams)
     console.log(searchParams.get('nombre'))
     console.log(searchParams.get('apellido'))
+
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`)
     const data = await res.json()
     return NextResponse.json(data)
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -7,7 +7,9 @@ export async function GET() {
   return NextResponse.json(data)
 }
 
-export function POST() {
+export async function POST(request) {
+  const data = await request.json()
+  console.log(data)
   return NextResponse.json({
     message: "creando datos!"
   })
