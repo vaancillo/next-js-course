60521484c11df05170b684019f1d530033f4df2a
vancillo <yaredbane@gmail.com>
2023-09-01 23:17:05 -0600

    enviroment variables

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server"
 
+console.log(process.env.TOKEN)
+console.log(process.env.SECRET_KEY)
+
 export async function GET() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users?apikey=123${process.env.TOKEN}`)
+  console.log(`https://jsonplaceholder.typicode.com/users?apikey=${process.env.TOKEN}`)
   const data = await res.json()
 
   return NextResponse.json(data)
